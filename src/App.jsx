cbe498e0e49dacfe58939be41b921efc23b1bec1
ChongHinYu <mailtohin274@hotmail.com>
2023-10-16 16:03:36 +0100

    Added more cart props to header component

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
   return (
     <>
 
-      <Header openCart={openCart} hasCartItems={hasCartItems} cart={cart} />
+      <Header openCart={openCart} hasCartItems={hasCartItems} cart={cart} isCartOpen={isCartOpen} removeFromCart={removeFromCart} />
       <main>
         <ProductPage isCartOpen={isCartOpen} cart={cart} hasCartItems={hasCartItems} quantity={quantity} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} addToCart={addToCart} removeFromCart={removeFromCart} productName={productName} originalPrice={originalPrice} discount={discount} finalPrice={finalPrice} />
       </main>
