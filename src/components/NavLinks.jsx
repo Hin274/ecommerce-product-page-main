57879a22921d4b0e420842c8a13263fa7de7ce5f
ChongHinYu <mailtohin274@hotmail.com>
2023-10-16 16:08:21 +0100

    Add nav link styling

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -2,12 +2,12 @@
 
 const NavLinks = () => {
     return (
-        <ul className="text-main-black font-bold grid gap-4">
-            <li ><a href='#'>Collections</a></li>
-            <li><a href='#'>Men</a></li>
-            <li><a href='#'>Women</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Contact</a></li>
+        <ul className="text-main-black font-bold grid gap-4 lg:flex lg:text-grayish-blue lg:font-normal">
+            <li ><a className="hover:border-b-2 hover:border-main-orange hover:py-8" href='#'>Collections</a></li>
+            <li><a className="hover:border-b-2 hover:border-main-orange hover:py-8"  href='#'>Men</a></li>
+            <li><a className="hover:border-b-2 hover:border-main-orange hover:py-8" href='#'>Women</a></li>
+            <li><a className="hover:border-b-2 hover:border-main-orange hover:py-8" href='#'>About</a></li>
+            <li><a className="hover:border-b-2 hover:border-main-orange hover:py-8" href='#'>Contact</a></li>
         </ul>
         
     );
