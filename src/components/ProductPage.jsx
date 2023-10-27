e54bf535471dbc085bdfa65f850788ae0a9ee7cb
ChongHinYu <mailtohin274@hotmail.com>
2023-10-27 15:37:20 +0100

    Add hover effect on button

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -82,11 +82,11 @@ const ProductPage = ({ isCartOpen, cart, hasCartItems, quantity, increaseQuantit
                 </div>
                 <div className="flex flex-col gap-5 lg:flex-row">
                     <div className="flex justify-between items-center bg-light-grayish-blue p-4 px-6 rounded-lg lg:basis-[40%]">
-                        <button aria-label="Decrease quantity" onClick={decreaseQuantity}><img src="./images/icon-minus.svg" alt="minus-icon" /></button>
+                        <button className="hover:opacity-70" aria-label="Decrease quantity" onClick={decreaseQuantity}><img src="./images/icon-minus.svg" alt="minus-icon" /></button>
                         <p className="font-bold">{quantity}</p>
-                        <button aria-label="Increase quantity" onClick={increaseQuantity}><img src="./images/icon-plus.svg" alt="plus-icon" /></button>
+                        <button className="hover:opacity-70" aria-label="Increase quantity" onClick={increaseQuantity}><img src="./images/icon-plus.svg" alt="plus-icon" /></button>
                     </div>
-                    <button onClick={addToCart} className="flex justify-center items-center bg-main-orange rounded-lg p-4 text-main-white font-bold gap-4 lg:basis-[60%]"><svg width="20" height="18" className="fill-main-white" viewBox="0 0 22 22"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" /></svg>Add to cart</button>
+                    <button onClick={addToCart} className="flex justify-center items-center bg-main-orange rounded-lg p-4 text-main-white font-bold gap-4 lg:basis-[60%] hover:opacity-70"><svg width="20" height="18" className="fill-main-white" viewBox="0 0 22 22"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" /></svg>Add to cart</button>
                 </div>
             </div>
         </div>
